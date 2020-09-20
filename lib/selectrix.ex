3ed1486cfdae4b23fe95517d25f613161e93cbc6
ityonemo <isaac.yonemoto@gmail.com>
2020-09-19 18:29:03 -0700

    first commit with new elixir project boilerplate

---
+++ lib/selectrix.ex
@@ -0,0 +1,18 @@
+defmodule Selectrix do
+  @moduledoc """
+  Documentation for `Selectrix`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Selectrix.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/selectrix_test.exs
@@ -0,0 +1,8 @@
+defmodule SelectrixTest do
+  use ExUnit.Case
+  doctest Selectrix
+
+  test "greets the world" do
+    assert Selectrix.hello() == :world
+  end
+end
